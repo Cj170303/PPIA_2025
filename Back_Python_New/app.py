2b110bc9258ae00137f1fe4139d073ca7f39e647
Camilo Jose <c.carrascalg@uniandes.edu.co>
2025-03-04 12:12:56 -0500

    Latex 1.1.0

--- Back_Python_New/app.py
+++ Back_Python_New/app.py
@@ -345,7 +345,7 @@ def receive_question():
                 responseChatbot = "reinit"
             elif responseStudent.lower() in ["no"]:
                 # Ejemplo: se podría mostrar una imagen de salida
-                responseChatbot = os.path.join('exit.png')
+                responseChatbot = os.path.join('Back_Python_New/exit.png')
             else:
                 responseChatbot = "No entendí tu respuesta. ¿ Desea reiniciar un quiz ?"
 
